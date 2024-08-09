b8d3a100b277a03946ef0b95c935e436151b5842
Ankit Saini <as.ankitsaini406@gmail.com>
2024-08-09 16:43:01 +0530

    "filter button"

--- src/components/filterbutton.jsx
+++ src/components/filterbutton.jsx
@@ -1,15 +1,25 @@
-
+import { useDispatch, useSelector } from "react-redux";
+import { filterTodo, markAllCompleted } from "../state/actions/actions";
 
 const FilterButton = () => {
+
+    const dispatch = useDispatch();
+    const currentFilter = useSelector((state) => state.filter);
+
+    const handleFilter = (filter) => {
+        dispatch(filterTodo(filter))
+    }
+
+
     return (
         <div>
-            <select name="" id="">
+            <select value={currentFilter} onChange={(e) => handleFilter(e.target.value)}>
                 <option value="ALL">Default</option>
                 <option value="COMPLETED">Completed</option>
                 <option value="INCOMPLET">InComplet</option>
             </select>
 
-            <button>Mark All Completed</button>
+            <button onClick={() => markAllCompleted()}>Mark All Completed</button>
         </div>
     )
 }
